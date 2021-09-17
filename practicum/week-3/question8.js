0d1be473ed4048b1eb597dff18049b9bfdc595b7
TheCurrySauseBandit <elementalspear@gmail.com>
2021-09-17 01:56:31 -0700

    question 8 migh tbe finished.

--- practicum/week-3/question8.js
+++ practicum/week-3/question8.js
@@ -14,4 +14,24 @@ output: 3
 
 var d_integer = function(input) {
 
+    var currNum = 0;
+    var currCount = 0;
+    for (var i = 0; i < input.length; i++) {
+        for (var j = i; j < input.length; j++) {
+            console.log(input[i] + " " + input[j]);
+            if (input[i] == input[j]) {
+                currCount += 1;
+            }
+            
+            
+        }
+        if (currCount == input[i]) {
+            return input[i];
+        }
+        currCount = 0;
+    }
+    return -1;
 };
+
+//let test8 = d_integer([3,5,3,3,5,1]);
+//console.log(test8);

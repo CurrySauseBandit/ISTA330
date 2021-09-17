986c969569344280cfdca304ede160bf03d030f6
TheCurrySauseBandit <elementalspear@gmail.com>
2021-09-17 01:42:36 -0700

    q6finish

--- practicum/week-3/question6.js
+++ practicum/week-3/question6.js
@@ -13,4 +13,21 @@ output: 2
 
 var m_element = function(input) {
 
+    var currNum = 0;
+    var currCount = 0;
+    for (var i = 0; i < input.length; i++) {
+        for (var j = i + 1; j < input.length; j++) {
+            console.log(input[i] + " " + input[j]);
+            if (input[i] == input[j]) {
+                currCount += 1;
+            }
+            if (currCount >= 3) {
+                return input[i];
+            }
+            
+        }
+        currCount = 0;
+    }
+    return -1;
 };
+

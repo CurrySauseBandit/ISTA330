6241f2a0d58d76ec1eae5e2c9c271fc3aed7b9d7
TheCurrySauseBandit <elementalspear@gmail.com>
2021-09-17 00:09:00 -0700

    testpush

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -10,6 +10,26 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
-     
-    
+     var currSum = 0;
+     var savedSum = 0;
+     var firstsum = true;
+
+     for (var i = 0; i < input.length(); i++) {
+        for (var j = i + 1; j < input.length(); j++) {
+                currSum += input[j];
+        }
+        currSum += input[i];
+        if (firstsum == true) {
+                savedSum = currSum;
+                firstsum = false;
+        }
+        if (currSum > savedSum) {
+                savedSum = currSum;
+        }
+        currSum = 0;
+        console.log(savedSum);
+     }
+     return savedSum;
  };
+
+
